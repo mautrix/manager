557e1dfba06efbe2876e7d9707606e5a02b4daa1
Tulir Asokan <tulir@maunium.net>
2024-07-28 20:09:26 +0300

    Remove rpm maker

--- forge.config.ts
+++ forge.config.ts
@@ -2,7 +2,6 @@ import type { ForgeConfig } from "@electron-forge/shared-types"
 import { MakerSquirrel } from "@electron-forge/maker-squirrel"
 import { MakerZIP } from "@electron-forge/maker-zip"
 import { MakerDeb } from "@electron-forge/maker-deb"
-import { MakerRpm } from "@electron-forge/maker-rpm"
 import { VitePlugin } from "@electron-forge/plugin-vite"
 import { FusesPlugin } from "@electron-forge/plugin-fuses"
 import { FuseV1Options, FuseVersion } from "@electron/fuses"
@@ -22,7 +21,6 @@ const config: ForgeConfig = {
 	makers: [
 		new MakerSquirrel({}),
 		new MakerZIP({}, ["darwin"]),
-		new MakerRpm({}),
 		new MakerDeb({
 			options: {
 				mimeType: ["x-scheme-handler/mautrix-manager"],
