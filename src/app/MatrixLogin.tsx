2acc1ce74bed0c96bd6836a43213db8af4d19301
Tulir Asokan <tulir@maunium.net>
2024-08-02 15:42:00 +0300

    Add CSS for Matrix login screen

--- src/app/MatrixLogin.tsx
+++ src/app/MatrixLogin.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from "react"
 import type { RespClientWellKnown, RespLogin } from "../types/matrix"
 import type { MatrixClient } from "../api/matrixclient"
+import "./MatrixLogin.css"
 
 interface LoginScreenProps {
 	homeserverURL: string
@@ -33,8 +34,12 @@ const LoginScreen = ({
 		)
 	}, [username, password, onLoggedIn, matrixClient])
 	const loginSSO = useCallback(() => {
-		window.mautrixAPI.openInBrowser(matrixClient.ssoRedirectURL)
-			.catch(err => window.alert(`Failed to open SSO URL in browser: ${err}`))
+		if (window.mautrixAPI.isDevBuild) {
+			window.location.href = matrixClient.ssoRedirectURL
+		} else {
+			window.mautrixAPI.openInBrowser(matrixClient.ssoRedirectURL)
+				.catch(err => window.alert(`Failed to open SSO URL in browser: ${err}`))
+		}
 	}, [matrixClient])
 
 	const resolveHomeserver = useCallback(() => {
@@ -75,47 +80,50 @@ const LoginScreen = ({
 			resolveHomeserver()
 			return
 		}
-		const timeout = setTimeout(resolveHomeserver, 1000)
+		const timeoutMS = homeserverURL.includes(".") ? 500 : 2000
+		const timeout = setTimeout(resolveHomeserver, timeoutMS)
 		return () => {
 			clearTimeout(timeout)
 		}
 	}, [homeserverURL, resolveHomeserver, forceResolveImmediate])
 
-	return (
-		<div>
-			Login
+	return <main className="matrix-login">
+		<h1>mautrix-manager</h1>
+		<input
+			type="text"
+			id="mxlogin-homeserver-url"
+			placeholder="Homeserver URL"
+			value={homeserverURL}
+			onChange={evt => {
+				setHomeserverURL(evt.target.value)
+				setLoginFlows(null)
+			}}
+		/>
+		{!loginFlows?.size && <div tabIndex={0} className="tab-catcher" />}
+		{loginFlows?.has("m.login.password") && <form onSubmit={login}>
 			<input
 				type="text"
-				id="homeserver-url"
-				value={homeserverURL}
-				onChange={evt => {
-					setHomeserverURL(evt.target.value)
-					setLoginFlows(null)
-				}}
+				id="mxlogin-username"
+				placeholder="Username"
+				value={username}
+				onChange={evt => setUsername(evt.target.value)}
 			/>
-			{loginFlows?.has("m.login.password") && <form onSubmit={login}>
-				<input
-					type="text"
-					id="username"
-					value={username}
-					onChange={evt => setUsername(evt.target.value)}
-				/>
-				<input
-					type="password"
-					id="password"
-					value={password}
-					onChange={evt => setPassword(evt.target.value)}
-				/>
-				<button type="submit">Login</button>
-			</form>}
-			{loginFlows?.has("m.login.sso") && <>
-				<button onClick={loginSSO}>SSO login</button>
-			</>}
-			{error && <>
-				error :( {error}
-			</>}
-		</div>
-	)
+			<input
+				type="password"
+				id="mxlogin-password"
+				placeholder="Password"
+				value={password}
+				onChange={evt => setPassword(evt.target.value)}
+			/>
+			<button className="mx-login-button" type="submit">Login</button>
+		</form>}
+		{loginFlows?.has("m.login.sso") && <>
+			<button className="mx-login-button" onClick={loginSSO}>SSO login</button>
+		</>}
+		{error && <div className="error">
+			{error}
+		</div>}
+	</main>
 }
 
 export default LoginScreen
